330e3f73785bbbda06cd11fdab9ab7ffbb9a3563
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-04-30 02:39:38 -0600

    feat: add validations for product fields with custom annotations for code and description

--- src/main/java/com/alfonsovidrio/springboot/app/springboot_crud/entities/Product.java
+++ src/main/java/com/alfonsovidrio/springboot/app/springboot_crud/entities/Product.java
@@ -1,10 +1,17 @@
 package com.alfonsovidrio.springboot.app.springboot_crud.entities;
 
+import com.alfonsovidrio.springboot.app.springboot_crud.validation.IsExistsDb;
+import com.alfonsovidrio.springboot.app.springboot_crud.validation.IsRequired;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "products")
@@ -13,8 +20,20 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @IsExistsDb
+    @IsRequired(message = "{IsRequired.product.code}")
+    private String code;
+
+    @NotBlank(message = "{NotBlank.product.name}")
+    @Size(min = 3, max = 50, message = "{Size.product.name}")
     private String name;
+
+    @NotNull(message = "{NotNull.product.price}")
+    @Min(value = 10, message = "{Min.product.price}")
     private Double price;
+
+    @IsRequired(message = "{IsRequired.product.description}")
+    @Size(min = 10, max = 255, message = "{Size.product.description}")
     private String description;
     
     public Product() {
@@ -53,4 +72,14 @@ public class Product {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    
 }
