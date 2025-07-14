4493ff39a65bbec582e5cef79f09086d783e21d3
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-07-13 23:10:23 -0600

    feat: add CORS and restrict user creation endpoint to ADMIN role

--- src/main/java/com/alfonsovidrio/springboot/app/springboot_crud/controllers/UserController.java
+++ src/main/java/com/alfonsovidrio/springboot/app/springboot_crud/controllers/UserController.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,8 @@ import com.alfonsovidrio.springboot.app.springboot_crud.services.UserService;
 
 import jakarta.validation.Valid;
 
+
+@CrossOrigin(origins = "https://localhost:3000", originPatterns = "*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -30,16 +34,18 @@ public class UserController {
         return service.findAll();
     }
 
-    @PostMapping
-    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result) {
-        if (result.hasFieldErrors()) return validation(result);
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
-    }
-
+    
     @PostMapping("/register")
     public ResponseEntity<?> register(@Valid @RequestBody User user, BindingResult result) {
         user.setAdmin(false);
         return create(user, result);
+    }
+    
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result) {
+        if (result.hasFieldErrors()) return validation(result);
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
     }
 
     private ResponseEntity<?> validation(BindingResult result) {
